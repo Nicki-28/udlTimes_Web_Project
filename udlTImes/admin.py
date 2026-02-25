045ae29c0d6b90d9ebec606c8d5deedd4a07fe38
PolMirasso <pol.mirasso@gmail.com>
2026-02-25 18:23:41 +0100

    added models

--- udlTImes/admin.py
+++ udlTImes/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import (
+    User,
+    Wordle,
+    StatsWordle,
+    Framed,
+    StatsFramed,
+    FramedGameData,
+    Connections,
+    ConnectionsWord,
+    StatsConnections,
+)
+
+admin.site.register(User)
+admin.site.register(Wordle)
+admin.site.register(StatsWordle)
+admin.site.register(Framed)
+admin.site.register(StatsFramed)
+admin.site.register(FramedGameData)
+admin.site.register(Connections)
+admin.site.register(ConnectionsWord)
+admin.site.register(StatsConnections)
--- udlTImes/models.py
+++ udlTImes/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+# User
 
-# Create your models here.
+class User(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Wordle
+
+class Wordle(models.Model):
+    word = models.CharField(max_length=100)
+    date = models.DateField(unique=True)
+
+
+class StatsWordle(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Wordle, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
+
+# Framed
+
+class Framed(models.Model):
+    id_paraula = models.CharField(max_length=100, unique=True)
+    paraula = models.CharField(max_length=100)
+    date = models.DateField(unique=True)
+
+
+class StatsFramed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_game = models.ForeignKey(Framed, on_delete=models.CASCADE)
+    value = models.CharField(max_length=100)
+
+
+class FramedGameData(models.Model):
+    id_game = models.ForeignKey(Framed, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    image = models.CharField(max_length=1000)
+
+
+# Connections
+
+class Connections(models.Model):
+    date = models.DateField(unique=True)
+
+
+class ConnectionsWord(models.Model):
+    id_category = models.ForeignKey(Connections, on_delete=models.CASCADE)
+    word = models.CharField(max_length=100)
+
+
+class StatsConnections(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Connections, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
