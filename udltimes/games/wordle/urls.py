5afd284f21ab8d9574b021da8b21a36e54fd3e72
nicole <nickiissj@gmail.com>
2026-04-01 22:35:50 +0200

    feat: add udltimes import to settings.py

--- udltimes/games/wordle/urls.py
+++ udltimes/games/wordle/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.wordle_view, name='wordle_view'),
-
+    path('', views.wordle_view, name='wordle'),
     path('check-guess/', views.check_guess, name='check_guess'),
+    path('daily/', views.dailyWordle, name='dailyWordle'),
 ]
