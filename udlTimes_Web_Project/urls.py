5bef9de6525cc1b0f017958257e4f229cbd9d1a0
PolMirasso <pol.mirasso@gmail.com>
2026-05-05 20:12:41 +0200

    Revert "Merge branch 'main' into integrate-games"
    This reverts commit a7690df21c440bf7fd5bb9aa7b2b743d07de064a, reversing
    changes made to 12d83cea6d3097a0c0cc0dc2d4337229e9c19b4a.

--- udlTimes_Web_Project/urls.py
+++ udlTimes_Web_Project/urls.py
@@ -8,17 +8,9 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from django.contrib.auth.views import LogoutView
-
+from templates.wordle.views import check_guess, dailyWordle
 from udltimes.views import (
     SignUpView,
-    home,
-    wordle_view,
-    connections_view,
-    framed_view,
-    register_view,
-    login_view,
-    profile_view,
     api_connections_complete,
     api_connections_guess,
     api_connections_today,
@@ -27,12 +19,14 @@ from udltimes.views import (
     api_wordle_guess,
     api_wordle_today,
     connections_save_view,
-    framed_autocomplete,
-    framed_api,
-    framed_save_api,
+    connections_view,
+    framed_view,
+    home,
+    login_view,
+    profile_view,
+    register_view,
+    wordle_view,
 )
-
-from udltimes.wordle.views import check_guess, dailyWordle
 
 urlpatterns = [
     path('wordle/', wordle_view, name='wordle'),
