67dae831ed95ccbb240639ea231add3aa10c2b84
PolMirasso <pol.mirasso@gmail.com>
2026-05-15 11:51:06 +0200

    hotfix() behave

--- udlTimes_Web_Project/settings.py
+++ udlTimes_Web_Project/settings.py
@@ -25,12 +25,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'udltimes',
-    "behave_django",
 ]
 
 # Add testing apps dynamically if TESTING mode is ON
-if TESTING:
-    INSTALLED_APPS.append('behave_django')
+try:
+    import behave_django  # noqa
+    INSTALLED_APPS.append('behave-django')
+except ImportError:
+    pass
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
